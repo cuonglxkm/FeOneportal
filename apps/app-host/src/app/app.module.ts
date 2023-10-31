004e4381dfaf17e1cc5e1786254ba3e0381748ee
tunglethanh <tunglethanh@vnpt.vn>
2023-10-31 16:30:55 +0700

    init project

--- apps/app-host/src/app/app.module.ts
+++ apps/app-host/src/app/app.module.ts
@@ -4,12 +4,24 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import {NzMenuModule} from "ng-zorro-antd/menu";
+import {NzLayoutModule} from "ng-zorro-antd/layout";
+import {IconsProviderModule} from "./icons-provider.module";
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    IconsProviderModule,
+    NzLayoutModule,
+    NzMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent],
