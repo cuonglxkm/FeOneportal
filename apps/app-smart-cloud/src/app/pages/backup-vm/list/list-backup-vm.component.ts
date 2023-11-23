376a95774ead1d9d93979a6f82f273ecf8129945
binnie <lananh4415@gmail.com>
2023-11-23 16:50:50 +0700

    lấy danh sách backup vm

--- apps/app-smart-cloud/src/app/pages/backup-vm/list/list-backup-vm.component.ts
+++ apps/app-smart-cloud/src/app/pages/backup-vm/list/list-backup-vm.component.ts
@@ -1,6 +1,12 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
 import {RegionModel} from "../../../shared/models/region.model";
 import {ProjectModel} from "../../../shared/models/project.model";
+import {BackupVm, BackupVMFormSearch} from "../../../shared/models/backup-vm";
+import {BackupVmService} from "../../../shared/services/backup-vm.service";
+import Pagination from "../../../shared/models/pagination";
+import {AllowAddressPair} from "../../../shared/models/allow-address-pair";
+import {NzTableQueryParams} from "ng-zorro-antd/table";
+import {DA_SERVICE_TOKEN, ITokenService} from "@delon/auth";
 
 @Component({
   selector: 'one-portal-list-backup-vm',
@@ -9,9 +15,9 @@ import {ProjectModel} from "../../../shared/models/project.model";
 })
 export class ListBackupVmComponent implements OnInit {
 
-  region: number;
+  region = JSON.parse(localStorage.getItem('region')).regionId;
 
-  project: number;
+  project = JSON.parse(localStorage.getItem('projectId'));
 
   value?: string;
 
@@ -23,10 +29,24 @@ export class ListBackupVmComponent implements OnInit {
     {label: 'Tất cả', value: 'all'},
     {label: 'Hoạt động', value: 'AVAILABLE'},
     {label: 'Tạm dừng', value: 'SUSPENDED'},
-    {label: 'Lỗi', value:'ERROR'}
+    {label: 'Lỗi', value: 'ERROR'}
   ]
 
-  selectedValue: any;
+  selectedValue: null
+
+  formSearch: BackupVMFormSearch = new BackupVMFormSearch()
+
+  collection: Pagination<BackupVm> = {
+    previousPage: 0,
+    totalCount: 0,
+    records: [],
+    currentPage: 1,
+    pageSize: 10
+  };
+
+  constructor(private backupVmService: BackupVmService,
+              @Inject(DA_SERVICE_TOKEN) private tokenService: ITokenService) {
+  }
 
   regionChanged(region: RegionModel) {
     this.region = region.regionId
@@ -38,6 +58,7 @@ export class ListBackupVmComponent implements OnInit {
 
   onInputChange(value: string) {
     this.value = value;
+    console.log('input text: ', this.value)
   }
 
   showModalDelete(): void {
@@ -48,20 +69,54 @@ export class ListBackupVmComponent implements OnInit {
     this.isVisibleDelete = false;
   }
 
-  handleOkDelete(){
+  handleOkDelete() {
   }
 
-  search(){
+  getListBackupVM(formSearch: BackupVMFormSearch) {
+    this.isLoading = true;
+    this.backupVmService.search(formSearch).subscribe(data => {
+      this.isLoading = false
+      this.collection = data
+      console.log(this.collection)
+    })
   }
 
-  getListBackupVM() {
-  }
-
-  onChange(value){
+  onChange(value) {
     this.selectedValue = value.value;
     console.log('selected', this.selectedValue)
+    if (this.selectedValue === 'all') {
+      this.formSearch.status = null
+    } else {
+      this.formSearch.status = this.selectedValue
+    }
+    this.getListBackupVM(this.formSearch)
+  }
+
+  onQueryParamsChange(params: NzTableQueryParams) {
+    const {pageSize, pageIndex} = params
+    this.formSearch.pageSize = pageSize;
+    this.formSearch.currentPage = pageIndex
+    this.getListBackupVM(this.formSearch);
   }
 
   ngOnInit(): void {
+    console.log(this.region)
+    this.formSearch.regionId = this.region
+
+    // this.formSearch.customerId = this.tokenService.get()?.userId
+    // this.formSearch.projectId = this.project
+
+    this.formSearch.customerId = null
+    this.formSearch.projectId = null
+
+
+    if (this.value === undefined) {
+      this.formSearch.instanceBackupName = null
+    } else {
+      this.formSearch.instanceBackupName = this.value
+    }
+    this.formSearch.pageSize = 10
+    this.formSearch.currentPage = 1
+    console.log(this.formSearch)
   }
 }
--- apps/app-smart-cloud/src/app/shared/services/backup-vm.service.ts
+++ apps/app-smart-cloud/src/app/shared/services/backup-vm.service.ts
@@ -1,6 +1,8 @@
 import {Injectable} from "@angular/core";
 import {BaseService} from "./base.service";
-import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
+import {BackupVm, BackupVMFormSearch} from "../models/backup-vm";
+import Pagination from "../models/pagination";
 
 @Injectable({
   providedIn: 'root'
@@ -18,5 +20,30 @@ export class BackupVmService extends BaseService {
     })
   }
 
+  search(form: BackupVMFormSearch) {
+    let params = new HttpParams();
+    if(form.customerId != null){
+      params = params.append('customerId', form.customerId);
+    }
+    if(form.projectId !=null) {
+      params = params.append('projectId', form.projectId);
+    }
+    if(form.regionId != null) {
+      params = params.append('regionId', form.regionId);
+    }
+    if (form.status != null) {
+      params = params.append('status', form.status);
+    }
+    if (form.instanceBackupName != null) {
+      params = params.append('instanceBackupName', form.instanceBackupName);
+    }
+    params = params.append('pageSize', form.pageSize);
+    params = params.append('currentPage', form.currentPage);
+
+    return this.http.get<Pagination<BackupVm>>(this.baseUrl + this.ENDPOINT.provisions + '/backups/intances', {
+      headers: this.getHeaders(),
+      params: params
+    })
+  }
 
 }
