af840c108041fcc1bcf080c831556da043d94273
nguyenhuuvu <vunh2@vnpt.vn>
2023-12-06 17:22:38 +0700

    create Users

--- apps/app-host/_mock/index.ts
+++ apps/app-host/_mock/index.ts
@@ -2,4 +2,6 @@ export * from './_profile';
 export * from './_ip-public';
 export  * from './_getInstanceBySecurityGroup'
 export  * from './_action-history'
+export * from './_user'
+export * from './_groupCreateUser'
 
