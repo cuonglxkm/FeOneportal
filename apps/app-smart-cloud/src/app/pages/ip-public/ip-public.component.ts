b78c14c431859c8db7959e741ca17c1d2aeb735c
sonch <sonch@vnpt.vn>
2023-12-18 12:45:51 +0700

    push code

--- apps/app-smart-cloud/src/app/pages/ip-public/ip-public.component.ts
+++ apps/app-smart-cloud/src/app/pages/ip-public/ip-public.component.ts
@@ -1,41 +1,52 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
 import {IpPublicService} from "../../shared/services/ip-public.service";
 import {RegionModel} from "../../shared/models/region.model";
 import {ProjectModel} from "../../shared/models/project.model";
 import {BaseResponse} from "../../../../../../libs/common-utils/src";
 import {IpPublicModel} from "../../shared/models/ip-public.model";
 import {Router} from "@angular/router";
+import {DA_SERVICE_TOKEN, ITokenService} from "@delon/auth";
+import {InstancesService} from "../instances/instances.service";
+import {NzMessageService} from "ng-zorro-antd/message";
+import {finalize} from "rxjs/operators";
 
 @Component({
   selector: 'one-portal-ip-public',
   templateUrl: './ip-public.component.html',
   styleUrls: ['./ip-public.component.less'],
 })
-export class IpPublicComponent implements OnInit{
+export class IpPublicComponent implements OnInit {
   regionId: number;
   projectId: number;
   listOfIp: IpPublicModel[] = [];
   checkEmpty: IpPublicModel[] = [];
   isBegin: Boolean = false;
   size = 10;
-  index: number = 0;
+  index: number = 1;
   total: number = 0;
   baseResponse: BaseResponse<IpPublicModel[]>;
   ipAddress: any = '';
-  status: any = 0;
-  customerId: any = 669;
-  isCheckState: any = false;
+  isCheckState: any = true;
   actionIp: any;
   selectedAction = 'Gắn Ip Pulbic';
-  selectedStatus = 'Tất cả trạng thái';
+  selectedStatus = '';
   isVisibleMounted: boolean = false;
   isVisibleRemove: boolean = false;
   isVisibleDelete: boolean = false;
-  id : any;
+  listInstance: any[];
+  instanceSelected;
+  id: any;
 
-  statusData = ['Tất cả trạng thái','Gắn Ip Pulbic', 'Gỡ Ip Pulbic', 'Xóa'];
+  statusData = [
+    {name: 'Tất cả trạng thái', value: ''},
+    {name: 'Khởi tạo', value: '0'},
+    {name: 'Đang sử dụng', value: '2'}];
   actionData = ['Gắn Ip Pulbic', 'Gỡ Ip Pulbic', 'Xóa'];
-  constructor(private service: IpPublicService, private router: Router) {
+
+  constructor(private service: IpPublicService, private router: Router,
+              @Inject(DA_SERVICE_TOKEN) private tokenService: ITokenService,
+              private instancService: InstancesService,
+              private message: NzMessageService,) {
 
   }
 
@@ -44,60 +55,53 @@ export class IpPublicComponent implements OnInit{
     'border-radius': '10px',
     'width': '1000px',
   };
+  loading = false;
 
   ngOnInit(): void {
     this.service.model.subscribe(data => {
-        console.log(data)
+      console.log(data)
     })
-    this.getData();
   }
 
-  ngOnChange(): void{
-    if (this.actionIp === 0) {
-      this.isVisibleMounted = true;
-    } else if (this.actionIp === 1) {
-
-    } else if (this.actionIp === 2) {
-
-    }
-  }
   getData(): void {
-    // this.service.getData(this.ipAddress, this.status, this.customerId, this.regionId, this.isCheckState, this.size, this.index)
+    this.loading = true;
+    this.service.getData(this.ipAddress, this.selectedStatus, this.tokenService.get()?.userId, this.regionId, this.isCheckState, this.size, this.index)
+      .pipe(finalize(() => this.loading = false))
+      .subscribe(baseResponse => {
+        this.listOfIp = baseResponse.records;
+        console.log(this.listOfIp);
+      });
+    // this.service.getTest()
     //   .subscribe(baseResponse => {
     //   this.listOfIp = baseResponse.records;
     //     console.log(this.listOfIp);
     // });
-    this.service.getTest()
-      .subscribe(baseResponse => {
-      this.listOfIp = baseResponse.records;
-        console.log(this.listOfIp);
-    });
   }
 
-  search(search: string) {
-    console.log(search);
-    // this.searchKey = search;
-    // this.getSshKeys();
+  search(inputSearch: any) {
+    if (inputSearch !== null) {
+      this.ipAddress = inputSearch;
+    }
+    this.getData();
   }
 
   onRegionChange(region: RegionModel) {
     this.regionId = region.regionId;
-    // this.getSshKeys();
+    this.getData();
   }
 
   projectChange(project: ProjectModel) {
     this.projectId = project.id;
-    // this.getSshKeys();
   }
 
   onPageSizeChange(event: any) {
-    // this.size = event
-    // this.getSshKeys();
+    this.size = event
+    this.getData();
   }
 
   onPageIndexChange(event: any) {
-    // this.index = event;
-    // this.getSshKeys();
+    this.index = event;
+    this.getData();
   }
 
   createIp() {
@@ -109,10 +113,16 @@ export class IpPublicComponent implements OnInit{
     this.isVisibleRemove = false;
     this.isVisibleDelete = false;
   }
+
   openIpMounted(event: any, id: any) {
     if (event == false) {
       this.id = id;
       if (this.selectedAction === 'Gắn Ip Pulbic') {
+        this.instancService.search(1, 999, this.regionId, '', '', this.tokenService.get()?.userId).subscribe(
+          (data) => {
+            this.listInstance = data.records;
+          }
+        );
         this.isVisibleMounted = true;
       } else if (this.selectedAction === 'Gỡ Ip Pulbic') {
         this.isVisibleRemove = true;
@@ -123,18 +133,57 @@ export class IpPublicComponent implements OnInit{
 
   }
 
-  openIpRemove(id: any) {
-    this.id = id;
-    this.isVisibleRemove = true;
+  openIpRemove() {
+    const request = {
+      id: this.id
+    }
+    this.service.attachIpPublic(request).subscribe(
+      {
+        next: post => {
+          this.message.create('success', `Xóa thành công Ip Public`);
+        },
+        error: e => {
+          this.message.create('error', `Xóa thất bại Ip Public`);
+        },
+      }
+    )
+    this.isVisibleRemove = false;
+    this.getData();
   }
 
-  openIpDelete(id: any) {
-    this.id = id;
-    this.isVisibleDelete = true;
+  openIpDelete() {
+    this.service.remove(this.id).subscribe(
+      {
+        next: post => {
+          this.message.create('success', `Xóa thành công Ip Public`);
+        },
+        error: e => {
+          this.message.create('error', `Xóa thất bại Ip Public`);
+        },
+      }
+    )
+    this.isVisibleDelete = false;
+    this.getData();
   }
 
   Mounted() {
     // call api
+    const request = {
+      id: this.id,
+      attachedVmId: this.instanceSelected,
+    }
+
+    this.service.attachIpPublic(request).subscribe(
+      {
+        next: post => {
+          this.message.create('success', 'Gắn thành công Ip Public');
+        },
+        error: e => {
+          this.message.create('error',  'Gắn thất bại IP Public');
+        },
+      }
+    )
     this.isVisibleMounted = false;
+    this.getData();
   }
 }
