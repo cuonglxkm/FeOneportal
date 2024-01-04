7d18aa1eb0fb01784d88d9d8919e59f8f83cdf75
dangminhnam <dangminhnam@vnpt.vn>
2024-01-04 15:23:52 +0700

    update code: fix userID API

--- apps/app-smart-cloud/src/app/shared/services/snapshot-volume.service.ts
+++ apps/app-smart-cloud/src/app/shared/services/snapshot-volume.service.ts
@@ -1,77 +1,84 @@
-import {BaseService} from "./base.service";
-import {HttpClient, HttpHeaders} from "@angular/common/http";
-import {Injectable} from "@angular/core";
-import {Observable, of} from "rxjs";
-import {EditTextVolumeModel, GetListVolumeModel} from "../models/volume.model";
-import {catchError} from "rxjs/operators";
-import {NzMessageService} from "ng-zorro-antd/message";
+import { BaseService } from './base.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { NzMessageService } from 'ng-zorro-antd/message';
 import {
   CreateScheduleSnapshotDTO,
   EditSnapshotVolume,
   GetListSnapshotVlModel,
-  ScheduleSnapshotVLDTO, SnapshotScheduleDetailDTO
-} from "../models/snapshotvl.model";
-import {VolumeDTO} from "../dto/volume.dto";
-import {SnapshotVolumeDto} from "../dto/snapshot-volume.dto";
-import {BaseResponse} from "../../../../../../libs/common-utils/src";
+  ScheduleSnapshotVLDTO,
+  SnapshotScheduleDetailDTO
+} from '../models/snapshotvl.model';
+import { SnapshotVolumeDto } from '../dto/snapshot-volume.dto';
+import { BaseResponse } from '../../../../../../libs/common-utils/src';
+import { DA_SERVICE_TOKEN, ITokenService } from '@delon/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SnapshotVolumeService extends BaseService {
 
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json' ,
+      'Authorization': 'Bearer ' + this.tokenService.get()?.token,
+      'user_root_id': this.tokenService.get()?.userId,
+    })
+  };
   //API GW
   private urlSnapshotVl = this.baseUrl + this.ENDPOINT.provisions + '/vlsnapshots';
 
 
   getSnapshotVolumes(customerId: number, projectId: number, regionId: number, size: number, pageSize: number, currentPage: number, status: string, volumeName: string, name: string): Observable<GetListSnapshotVlModel> {
     let urlResult = this.getConditionSearchSnapshotVl(customerId, projectId, regionId, size, pageSize, currentPage, status, volumeName, name);
-    return this.http.get<GetListSnapshotVlModel>(urlResult).pipe(
+    return this.http.get<GetListSnapshotVlModel>(urlResult,this.httpOptions).pipe(
       catchError(this.handleError<GetListSnapshotVlModel>('get snapshot-volume-list error'))
     );
   }
 
   getSnapshotVolummeById(snapshotVlID: string) {
-    return this.http.get<SnapshotVolumeDto>(this.urlSnapshotVl + '/' + snapshotVlID).pipe(
+    return this.http.get<SnapshotVolumeDto>(this.urlSnapshotVl + '/' + snapshotVlID,this.httpOptions).pipe(
       catchError(this.handleError<SnapshotVolumeDto>('get snapshot volume-detail error'))
     )
   }
 
   editSnapshotVolume(request: EditSnapshotVolume): Observable<any> {
-    return this.http.put(this.urlSnapshotVl, request).pipe(
+    return this.http.put(this.urlSnapshotVl, request,this.httpOptions).pipe(
       catchError(this.handleError<any>('Edit Snapshot Volume to VM error.'))
     );
   }
 
   deleteSnapshotVolume(idSnapshotVolume: number): Observable<boolean> {
-    return this.http.delete<boolean>(this.urlSnapshotVl + '/' + idSnapshotVolume).pipe(
+    return this.http.delete<boolean>(this.urlSnapshotVl + '/' + idSnapshotVolume,this.httpOptions).pipe(
       catchError(this.handleError<boolean>('delete snapshot volume error.'))
     );
   }
 
   createSnapshotSchedule(request: CreateScheduleSnapshotDTO): Observable<any>{
     let urlResult = this.urlSnapshotVl + '/schedule';
-    return this.http.post(urlResult, request).pipe(
+    return this.http.post(urlResult, request,this.httpOptions).pipe(
       catchError(this.handleError<any>('Create Snapshot schedule error.'))
     );
   }
 
   getDetailSnapshotSchedule(id: number, customerId: number):Observable<SnapshotScheduleDetailDTO>{
-    return this.http.get<SnapshotScheduleDetailDTO>(this.urlSnapshotVl + '/schedule/'  + id + '?customerId='+customerId).pipe(
+    return this.http.get<SnapshotScheduleDetailDTO>(this.urlSnapshotVl + '/schedule/'  + id + '?customerId='+customerId,this.httpOptions).pipe(
       catchError(this.handleError<SnapshotScheduleDetailDTO>('get snapshot snapshot-schedule-detail error'))
     )
   }
 
   getListSchedule(pageSize:number, currentPage:number, customerID: number, projectId: number, regionId: number, status: string): Observable<BaseResponse<ScheduleSnapshotVLDTO[]>>{
     let urlResult = this.getConditionSearchScheduleSnapshotVl(pageSize,currentPage,customerID,projectId,regionId,status)
-    return this.http.get<GetListSnapshotVlModel>(urlResult).pipe(
+    return this.http.get<GetListSnapshotVlModel>(urlResult,this.httpOptions).pipe(
       catchError(this.handleError<GetListSnapshotVlModel>('get shedule-snapshot-volume-list error'))
     );
   }
 
   actionSchedule(scheduleId: number, action:string, customerId: number, regionId: number, projectId: number): Observable<boolean>{
     let urlResult = this.urlSnapshotVl + '/schedule/' + scheduleId + '/action';
-    return this.http.post<boolean>(urlResult, null).pipe(
+    return this.http.post<boolean>(urlResult, null,this.httpOptions).pipe(
       catchError(this.handleError<boolean>('get shedule-snapshot-volume-list error'))
     );
   }
@@ -200,7 +207,7 @@ export class SnapshotVolumeService extends BaseService {
     return urlResult;
   }
 
-  constructor(private http: HttpClient , private message: NzMessageService) {
+  constructor(private http: HttpClient , private message: NzMessageService,@Inject(DA_SERVICE_TOKEN) private tokenService: ITokenService) {
     super();
   }
   private handleError<T>(operation = 'operation', result?: T) {
