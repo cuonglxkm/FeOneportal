34dbe36a92ff01f828f450710aba17ab6e5bc5be
tunglethanh <tunglethanh@vnpt.vn>
2024-01-08 11:51:55 +0700

    Revert "update to angular 17"
    This reverts commit 050864d9480648cf63fc531f55de333cc6b481b5.

--- apps/app-host/src/app/app.routes.ts
+++ apps/app-host/src/app/app.routes.ts
@@ -1,3 +1,4 @@
+import { NxWelcomeComponent } from './nx-welcome.component';
 import { Route } from '@angular/router';
 import { loadRemoteModule } from '@nx/angular/mf';
 
@@ -8,5 +9,17 @@ export const appRoutes: Route[] = [
       loadRemoteModule('app-smart-cloud', './Module').then(
         (m) => m.RemoteEntryModule
       ),
-  }
+  },
+  {
+    path: 'app-base',
+    loadChildren: () =>
+      loadRemoteModule('app-base', './Module').then(
+        (m) => m.RemoteEntryModule
+      ),
+  },
+  {
+    path: 'app-dashboard',
+    loadChildren: () =>
+      loadRemoteModule('app-dashboard', './Routes').then((m) => m.remoteRoutes),
+  },
 ];
