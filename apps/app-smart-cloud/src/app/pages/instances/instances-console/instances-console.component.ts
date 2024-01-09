0813a02888e814362a520ef59d840c183ab8a746
tunglethanh <tunglethanh@vnpt.vn>
2024-01-09 17:05:14 +0700

    fix fullscreen make not scrollable

--- apps/app-smart-cloud/src/app/pages/instances/instances-console/instances-console.component.ts
+++ apps/app-smart-cloud/src/app/pages/instances/instances-console/instances-console.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {FullContentService} from "@delon/abc/full-content";
 import {HttpClient} from "@angular/common/http";
 import {LoadingService, LoadingType} from "@delon/abc/loading";
@@ -11,7 +11,7 @@ import {InstancesService} from "../instances.service";
   templateUrl: './instances-console.component.html',
   styleUrls: ['./instances-console.component.less'],
 })
-export class InstancesConsoleComponent implements OnInit, AfterViewInit {
+export class InstancesConsoleComponent implements OnInit, AfterViewInit, OnDestroy {
   src: string = ''; // <- YOUR URL
   scroll = {y: '230px'};
   isFull: boolean = false;
@@ -27,6 +27,10 @@ export class InstancesConsoleComponent implements OnInit, AfterViewInit {
     private service: InstancesService) {
   }
 
+  ngOnDestroy(): void {
+
+  }
+
 
   ngOnInit(): void {
     this.vmId = this.route.snapshot.paramMap.get('id');
@@ -40,10 +44,13 @@ export class InstancesConsoleComponent implements OnInit, AfterViewInit {
   fullChange(val: boolean): void {
     this.isFull = val;
     this.scroll = val ? {y: '350px'} : {y: '230px'};
+
+
   }
 
   onClickFullScreen() {
-    this.fullContentService.toggle();
+    // this.fullContentService.toggle();
+    this.iframeEle.nativeElement.requestFullscreen()
     this.iframeEle.nativeElement.focus();
   }
 
