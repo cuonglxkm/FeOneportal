65d73ad570ab1b11df05fdf7cf131f11a7d5168c
nhiennd <nhiennd@vnpt.vn>
2024-01-30 14:09:11 +0700

    update route

--- apps/app-host/module-federation.config.ts
+++ apps/app-host/module-federation.config.ts
@@ -2,7 +2,7 @@ import { ModuleFederationConfig } from '@nx/webpack';
 
 const config: ModuleFederationConfig = {
   name: 'app-host',
-  remotes: ['app-kafka'],
+  remotes: [],
 };
 
 export default config;
