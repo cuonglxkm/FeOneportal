8d413edb56def0440ed48e5edca5abc03ceb5e37
nguyenhuuvu <huuvu2468@gmail.com>
2024-02-05 09:15:11 +0700

    fix bug thanh toán thất bại

--- apps/app-smart-cloud/src/app/shared/components/payment-success/payment-success.component.ts
+++ apps/app-smart-cloud/src/app/shared/components/payment-success/payment-success.component.ts
@@ -13,11 +13,15 @@ export class PaymentSuccessComponent implements OnInit {
   payment: PaymentModel;
   paymentCode = '';
   orderId: number;
+  paymentSuccess: boolean = false;
+  causeOfFail: string = '';
+
   constructor(
     private router: Router,
     private paymentService: PaymentService,
     private orderService: OrderService
   ) {}
+
   ngOnInit(): void {
     var data = window.location.search.replace('?', '').split('&');
     var dataString = data.find((x) => x.includes('data'));
@@ -25,6 +29,52 @@ export class PaymentSuccessComponent implements OnInit {
       dataString && dataString.split('=').length > 1
         ? dataString.split('=')[1]
         : null;
+
+    var resultString = data.find((x) => x.includes('vnptpayResponseCode'));
+    var resultCode =
+      resultString && resultString.split('=').length > 1
+        ? resultString.split('=')[1]
+        : null;
+    if (resultCode == '00') {
+      this.paymentSuccess = true;
+    } else {
+      this.paymentSuccess = false;
+      switch (resultCode) {
+        case '01':
+          this.causeOfFail = 'Giao dịch thất bại';
+          break;
+        case '02':
+          this.causeOfFail = 'Dữ liệu không đúng định dạng';
+          break;
+        case '03':
+          this.causeOfFail = 'Mã giao dịch đã tồn tại';
+          break;
+        case '04':
+          this.causeOfFail = 'Timeout';
+          break;
+        case '05':
+          this.causeOfFail = 'Không tìm thấy dữ liệu';
+          break;
+        case '06':
+          this.causeOfFail = 'Lỗi hệ thống';
+          break;
+        case '07':
+          this.causeOfFail = 'Chữ ký không đúng';
+          break;
+        case '08':
+          this.causeOfFail = 'Merchant service đang bị khóa';
+          break;
+        case '09':
+          this.causeOfFail = 'Merchant service không tồn tại';
+          break;
+        case '96':
+          this.causeOfFail = 'Hệ thống đang bảo trì';
+          break;
+        default:
+          this.causeOfFail = 'Lỗi không xác định';
+          break;
+      }
+    }
 
     this.paymentService
       .getPaymentByPaymentNumber(this.paymentCode)
