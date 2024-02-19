61c068833321bfaece3e6a835018ecbe57bee9b7
binnie <lananh4415@gmail.com>
2024-02-19 09:50:08 +0700

    fix ui theo prototype mới

--- apps/app-smart-cloud/src/app/pages/backup-vm/list/list-backup-vm.component.ts
+++ apps/app-smart-cloud/src/app/pages/backup-vm/list/list-backup-vm.component.ts
@@ -78,6 +78,7 @@ export class ListBackupVmComponent implements OnInit {
     onInputChange(value: string) {
         this.value = value;
         console.log('input text: ', this.value)
+        this.getListBackupVM()
     }
 
     showModalDelete(id: number): void {
