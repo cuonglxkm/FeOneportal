9cd14bf54f2994251410d3a1a0ab45351a60c5d9
nguyenhuuvu <huuvu2468@gmail.com>
2024-03-06 15:47:59 +0700

    create ui bucket

---
+++ apps/app-smart-cloud/src/app/pages/bucket/bucket-configure/bucket-configure.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'one-portal-bucket-configure',
+  templateUrl: './bucket-configure.component.html',
+  styleUrls: ['../bucket-list.component.less'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class BucketConfigureComponent {}
---
+++ apps/app-smart-cloud/src/app/pages/bucket/bucket-configure/lifecycle-config/lifecycle-config.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'one-portal-lifecycle-config',
+  templateUrl: './lifecycle-config.component.html',
+  styleUrls: ['./lifecycle-config.component.less'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class LifecycleConfigComponent {}
