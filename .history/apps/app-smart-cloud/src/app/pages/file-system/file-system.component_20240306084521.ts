85f8fad46bc90177133cf95e666007ecd5da81e6
DuongBT153717 <104844625+DuongBT153717@users.noreply.github.com>
2024-03-07 15:02:49 +0700

    update ui security group

--- .history/apps/app-smart-cloud/src/app/pages/file-system/file-system.component_20240306084521.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'one-portal-file-system',
-  templateUrl: './file-system.component.html',
-  styleUrls: ['./file-system.component.less'],
-})
-export class FileSystemComponent {}
