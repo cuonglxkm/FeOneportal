2640bf389103d0ab70cc20394a61e6f001398fd8
nguyenhuuvu <huuvu2468@gmail.com>
2024-05-18 17:04:44 +0700

    fix bug

--- apps/app-smart-cloud/src/app/pages/instances/instances-create/instances-create.component.ts
+++ apps/app-smart-cloud/src/app/pages/instances/instances-create/instances-create.component.ts
@@ -300,7 +300,7 @@ export class InstancesCreateComponent implements OnInit {
         debounceTime(300) // Đợi 500ms sau khi người dùng dừng nhập trước khi xử lý sự kiện
       )
       .subscribe((res) => {
-        this.dataService.checkExistName(res, this.region).subscribe((data) => {
+        this.dataService.checkExistName(res, this.region, this.projectId).subscribe((data) => {
           if (data == true) {
             this.isExistName = true;
           } else {
