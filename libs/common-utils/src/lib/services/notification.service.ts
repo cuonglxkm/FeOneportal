95ca1ce593102200667e78a1eb1f96884ef9fb2d
Lingg <nguyenquanglinh@vnpt.vn>
2024-05-29 17:51:14 +0700

    config lại đường dẫn

--- libs/common-utils/src/lib/services/notification.service.ts
+++ libs/common-utils/src/lib/services/notification.service.ts
@@ -14,7 +14,7 @@ export class NotificationService {
 
   public async initiateSignalrConnection(isRegisterGlobalMessage = false): Promise<void> {
 
-    let hubUrl = 'http://localhost:1019/notify';
+    let hubUrl = 'https://idg-api-gw-dev.onsmartcloud.com/notify';
 
     var tokenModel = this.tokenService.get();
 
