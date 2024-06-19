26cb7ff7dbfdd0dd16fb6471d71ed86161f5edae
binnie <lananh4415@gmail.com>
2024-06-19 10:20:46 +0700

    cập nật ui dashboard

--- apps/app-smart-cloud/src/app/pages/backup-package/detail/detail-package-backup.component.ts
+++ apps/app-smart-cloud/src/app/pages/backup-package/detail/detail-package-backup.component.ts
@@ -1,30 +1,32 @@
-import {Component, Inject, OnInit} from '@angular/core';
-import {ActivatedRoute, Router} from "@angular/router";
-import {PackageBackupService} from "../../../shared/services/package-backup.service";
-import {DA_SERVICE_TOKEN, ITokenService} from "@delon/auth";
-import {NzNotificationService} from "ng-zorro-antd/notification";
-import {NonNullableFormBuilder} from "@angular/forms";
-import {PackageBackupModel} from "../../../shared/models/package-backup.model";
-import {getCurrentRegionAndProject} from "@shared";
-import { RegionModel, ProjectModel } from '../../../../../../../libs/common-utils/src';
+import { Component, Inject, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PackageBackupService } from '../../../shared/services/package-backup.service';
+import { DA_SERVICE_TOKEN, ITokenService } from '@delon/auth';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
+import { NonNullableFormBuilder } from '@angular/forms';
+import { PackageBackupModel } from '../../../shared/models/package-backup.model';
+import { getCurrentRegionAndProject } from '@shared';
+import { ProjectModel, RegionModel } from '../../../../../../../libs/common-utils/src';
 import { ProjectService } from 'src/app/shared/services/project.service';
+import { ALAIN_I18N_TOKEN } from '@delon/theme';
+import { I18NService } from '@core';
 
 @Component({
   selector: 'one-portal-detail-package-backup',
   templateUrl: './detail-package-backup.component.html',
-  styleUrls: ['./detail-package-backup.component.less'],
+  styleUrls: ['./detail-package-backup.component.less']
 })
-export class DetailPackageBackupComponent implements OnInit{
+export class DetailPackageBackupComponent implements OnInit {
   region = JSON.parse(localStorage.getItem('regionId'));
   project = JSON.parse(localStorage.getItem('projectId'));
 
-  packageBackupModel: PackageBackupModel = new PackageBackupModel()
+  packageBackupModel: PackageBackupModel = new PackageBackupModel();
 
-  idPackageBackup: number
+  idPackageBackup: number;
 
-  typeVPC: number
+  typeVPC: number;
 
-  isLoading: boolean = false
+  isLoading: boolean = false;
 
   constructor(private router: Router,
               private packageBackupService: PackageBackupService,
@@ -32,59 +34,62 @@ export class DetailPackageBackupComponent implements OnInit{
               private notification: NzNotificationService,
               private route: ActivatedRoute,
               private fb: NonNullableFormBuilder,
-              private projectService: ProjectService) {
+              private projectService: ProjectService,
+              @Inject(ALAIN_I18N_TOKEN) private i18n: I18NService) {
   }
 
   regionChanged(region: RegionModel) {
-    this.region = region.regionId
-    this.projectService.getByRegion(this.region).subscribe(data => {
-      if (data.length) {
-        localStorage.setItem("projectId", data[0].id.toString())
-        this.router.navigate(['/app-smart-cloud/backup/packages'])
-      }
-    });
+    this.router.navigate(['/app-smart-cloud/backup/packages']);
   }
 
   projectChanged(project: ProjectModel) {
-    this.project = project?.id
+    this.project = project?.id;
+  }
+
+  userChanged(project: ProjectModel) {
+    this.router.navigate(['/app-smart-cloud/backup/packages']);
   }
 
   getDetailPackageBackup(id) {
-    this.isLoading = true
+    this.isLoading = true;
     this.packageBackupService.detail(id).subscribe(data => {
-      console.log('data', data)
-      this.isLoading = false
-      this.packageBackupModel = data
-    })
+      console.log('data', data);
+      this.isLoading = false;
+      this.packageBackupModel = data;
+    }, error => {
+      this.notification.error(this.i18n.fanyi('app.status.fail'), this.i18n.fanyi('app.backup.package.not.found'));
+      this.router.navigate(['/app-smart-cloud/backup/packages']);
+    });
   }
 
   navigateToExtend() {
-    this.router.navigate(['/app-smart-cloud/backup/packages/extend/' + this.idPackageBackup])
+    this.router.navigate(['/app-smart-cloud/backup/packages/extend/' + this.idPackageBackup]);
   }
 
   navigateToResize() {
-    this.router.navigate(['/app-smart-cloud/backup/packages/resize/' + this.idPackageBackup])
+    this.router.navigate(['/app-smart-cloud/backup/packages/resize/' + this.idPackageBackup]);
   }
 
   loadProjects() {
     this.projectService.getByRegion(this.region).subscribe(data => {
       let project = data.find(project => project.id === +this.project);
       if (project) {
-        this.typeVPC = project.type
+        this.typeVPC = project.type;
       }
     });
   }
+
   ngOnInit() {
-    this.idPackageBackup = Number.parseInt(this.route.snapshot.paramMap.get('id'))
-    let regionAndProject = getCurrentRegionAndProject()
-    this.region = regionAndProject.regionId
-    this.project = regionAndProject.projectId
+    this.idPackageBackup = Number.parseInt(this.route.snapshot.paramMap.get('id'));
+    let regionAndProject = getCurrentRegionAndProject();
+    this.region = regionAndProject.regionId;
+    this.project = regionAndProject.projectId;
     // this.customerId = this.tokenService.get()?.userId
     if (this.project && this.region) {
-      this.loadProjects()
+      this.loadProjects();
     }
-    if(this.idPackageBackup != null) {
-      this.getDetailPackageBackup(this.idPackageBackup)
+    if (this.idPackageBackup != null) {
+      this.getDetailPackageBackup(this.idPackageBackup);
     }
   }
 }
--- apps/app-smart-cloud/src/app/pages/dashboard/dashboard.component.ts
+++ apps/app-smart-cloud/src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, Inject, OnInit, ViewChild } from '@angular/core';
 import { DashboardService } from '../../shared/services/dashboard.service';
 import {
   DataChart,
@@ -9,6 +9,8 @@ import {
 import { BaseResponse } from '../../../../../../libs/common-utils/src';
 import { Router } from '@angular/router';
 import { Pie } from '@antv/g2plot';
+import { ALAIN_I18N_TOKEN } from '@delon/theme';
+import { I18NService } from '@core';
 
 
 @Component({
@@ -36,7 +38,8 @@ export class DashboardComponent implements OnInit {
   @ViewChild('pieChart', { static: true }) private pieChart: ElementRef;
 
   constructor(private dashboardService: DashboardService,
-              private router: Router) {
+              private router: Router,
+              @Inject(ALAIN_I18N_TOKEN) private i18n: I18NService) {
   }
 
   onPageSizeNearExpireChange(value) {
@@ -111,15 +114,10 @@ export class DashboardComponent implements OnInit {
       this.isLoading = false;
       this.dataPaymentCost = data;
 
-      const transformedArray = this.dataPaymentCost.map(item => ({
-        item: item.typeName,
-        count: item.totalAmount
-      }));
-      console.log('data', transformedArray);
-
       const dataChart = this.dataPaymentCost.map(item => ({
-        type: item.typeName,
-        value: item.totalAmount
+        type: this.getServiceName(item.serviceType),
+        value: item.totalAmount,
+        formattedValue: new Intl.NumberFormat('de-DE').format(item.totalAmount)
       }));
       const piePlot = new Pie(this.pieChart.nativeElement, {
         appendPadding: 10,
@@ -133,7 +131,17 @@ export class DashboardComponent implements OnInit {
           content: '{percentage}'
         },
         interactions: [{ type: 'element-active' }],
-        theme: 'custom-theme'
+        theme: 'custom-theme',
+        tooltip: {
+          formatter: (data) => {
+            const total = dataChart.reduce((sum, item) => sum + item.value, 0);
+            const percentage = ((data.value / total) * 100).toFixed(2);
+            return {
+              name: data.type,
+              value: `${new Intl.NumberFormat('de-DE').format(data.value)} (${percentage}%)`
+            };
+          }
+        }
       });
 
       piePlot.render();
@@ -152,16 +160,18 @@ export class DashboardComponent implements OnInit {
     switch (serviceType) {
       case 1:
         //VM
+        this.router.navigate(['/app-smart-cloud/instances/instances-extend/'+ serviceInstanceId])
         break;
       case 2:
         //VOLUME
-        this.router.navigate(['/app-smart-cloud/volumes/renew/', serviceInstanceId]);
+        this.router.navigate(['/app-smart-cloud/volumes/renew/' + serviceInstanceId]);
         break;
       case 3:
         //SNAPSHOT
         break;
       case 4:
         //IP PUBLIC
+        this.router.navigate(['/app-smart-cloud/ip-public/extend/' + serviceInstanceId])
         break;
       case 5:
         //LOAD BALANCER F5
@@ -186,6 +196,7 @@ export class DashboardComponent implements OnInit {
         break;
       case 12:
         //VPC
+        this.router.navigate(['/app-smart-cloud/project/extend/' + serviceInstanceId])
         break;
       case 13:
         //OBJECT STORAGE
@@ -205,8 +216,8 @@ export class DashboardComponent implements OnInit {
         //ROUTER
         break;
       case 18:
+        //FILE SYSTEM
         this.router.navigate(['/app-smart-cloud/file-storage/file-system/' + serviceInstanceId + '/extend']);
-        //SHAREFILE_STORAGE
         break;
       case 19:
         //K8s
@@ -224,8 +235,93 @@ export class DashboardComponent implements OnInit {
         //MONGO_DB
         break;
       case 24:
+        //FILE SYSTEM SNAPSHOT
+        break;
+      case 100:
         //OTHER
         break;
+      default:
+        break;
+    }
+  }
+
+  getServiceName(serviceType) {
+    switch (serviceType) {
+      case 1:
+        //VM
+        return this.i18n.fanyi('app.instances')
+      case 2:
+        //VOLUME
+        return this.i18n.fanyi('app.volume')
+      case 3:
+        //SNAPSHOT
+        return this.i18n.fanyi('app.snapshot')
+      case 4:
+        //IP PUBLIC
+        return this.i18n.fanyi('app.ip-public')
+      case 5:
+        //LOAD BALANCER F5
+        return this.i18n.fanyi('app.load-balancer.f5')
+      case 6:
+        // VM SNAPSHOT
+        return this.i18n.fanyi('app.vm.snapshot')
+      case 7:
+        //SNAPSHOT VOLUME
+        return this.i18n.fanyi('app.snapshot.volume')
+      case 8:
+        // BACKUP VOLUME
+        return this.i18n.fanyi('app.backup.volume')
+      case 9:
+        //BACKUP VM
+        return this.i18n.fanyi('app.backup.vm')
+      case 10:
+        //KEY PAIR
+        return this.i18n.fanyi('app.keypair')
+      case 11:
+        // SECURITY_GROUP
+        return this.i18n.fanyi('app.security.group')
+      case 12:
+        //VPC
+        return this.i18n.fanyi('app.vpc')
+      case 13:
+        //OBJECT STORAGE
+        return this.i18n.fanyi('app.object-storage')
+      case 14:
+        //BACKUP_PACKET
+        return this.i18n.fanyi('app.backup-package')
+      case 15:
+        //LOAD BALANCER SDN
+        return this.i18n.fanyi('app.load-balancer')
+      case 16:
+        //VLAN
+        return this.i18n.fanyi('app.vlan')
+      case 17:
+        //ROUTER
+        return this.i18n.fanyi('app.router')
+      case 18:
+        //FILE SYSTEM
+        return this.i18n.fanyi('app.file-system')
+      case 19:
+        //K8s
+        return this.i18n.fanyi('app.k8s')
+      case 20:
+        //VPN_SITE_TO_SITE
+        return this.i18n.fanyi('app.vpns2s')
+      case 21:
+        //KAFKA
+        return this.i18n.fanyi('app.kafka')
+      case 22:
+        //SNAPSHOT_PACKAGE
+        return this.i18n.fanyi('app.snapshot-package1')
+      case 23:
+        //MONGO_DB
+        return this.i18n.fanyi('app.mongo')
+      case 24:
+        //FILE SYSTEM SNAPSHOT
+        return this.i18n.fanyi('app.file-system-snapshot')
+      case 100:
+        //OTHER
+        return this.i18n.fanyi('app.other')
       default:
         break;
     }
