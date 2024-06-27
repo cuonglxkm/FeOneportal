a1d2883bfc8a72926e80312269ac67e1dbefb1cf
binnie <lananh4415@gmail.com>
2024-06-27 15:32:34 +0700

    cập nhật lịch

--- apps/app-smart-cloud/src/app/pages/dashboard/dashboard.component.ts
+++ apps/app-smart-cloud/src/app/pages/dashboard/dashboard.component.ts
@@ -407,8 +407,11 @@ export class DashboardComponent implements OnInit, OnDestroy {
       this.getDataChart();
     }, 1500)
     this.onChangeInputChange();
+    console.log('role', this.getUserRole())
     if(this.getUserRole().includes("SI")) {
       localStorage.setItem('role', 'SI')
+    } else {
+      localStorage.setItem('role', '')
     }
   }
 }
