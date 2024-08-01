259b365eda49b9af0a9d933db665945b56c88f22
buithaiduong <buithaiduong@vnpt.vn>
2024-08-01 15:46:32 +0000

    fix bug os

--- apps/app-smart-cloud/src/app/pages/bucket/bucket-detail/bucket-detail.component.ts
+++ apps/app-smart-cloud/src/app/pages/bucket/bucket-detail/bucket-detail.component.ts
@@ -352,6 +352,18 @@ export class BucketDetailComponent extends BaseService implements OnInit {
       );
   }
 
+  formatFileSize(size: number): string {
+    const units = ['bytes', 'KB', 'MB', 'GB', 'TB', 'PB'];
+    let unitIndex = 0;
+  
+    while (size >= 1024 && unitIndex < units.length - 1) {
+      size /= 1024;
+      unitIndex++;
+    }
+  
+    return `${size.toFixed(2)} ${units[unitIndex]}`;
+  }
+
   handleChange({ file, fileList }: NzUploadChangeParam) {
     let newFiles = fileList.filter(
       (item) =>
