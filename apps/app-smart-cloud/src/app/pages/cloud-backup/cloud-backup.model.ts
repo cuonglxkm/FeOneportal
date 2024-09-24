282918a28d2e4722d5de32c14eefdb1bac5957aa
cuongpv.it <cuongpv.it@vnpt.vn>
2024-09-24 07:57:56 +0000

    fix bug endpoint

--- apps/app-smart-cloud/src/app/pages/cloud-backup/cloud-backup.model.ts
+++ apps/app-smart-cloud/src/app/pages/cloud-backup/cloud-backup.model.ts
@@ -5,6 +5,7 @@ export class CloudBackup {
   capacity: number;
   status: string;
   createDate: Date;
+  createdDate?: Date;
   expiredDate: Date;
 }
 
@@ -17,4 +18,34 @@ export class AccessRule {
   id: number;
   port:number;
   source: string;
+}
+
+export class CloudBackupExtend {
+  regionId: number
+  serviceName: string
+  customerId: number
+  projectId: string
+  vpcId: string
+  typeName: string
+  serviceType: number
+  actionType: number
+  serviceInstanceId: number
+  newExpireDate: string
+  userEmail: string
+  actorEmail: string
+}
+
+export class CloudBackupResize {
+  newOfferId: number;
+  serviceType: number;
+  actionType: number;
+  serviceInstanceId: number;
+  regionId: number;
+  serviceName: string;
+  customerId: number;
+  projectId: string;
+  vpcId: string;
+  typeName: string;
+  userEmail: string;
+  actorEmail: string;
 }
--- apps/app-smart-cloud/src/app/pages/cloud-backup/extend/cloud-backup-extend.component.ts
+++ apps/app-smart-cloud/src/app/pages/cloud-backup/extend/cloud-backup-extend.component.ts
@@ -15,17 +15,16 @@ import {
 } from 'src/app/shared/models/object-storage.model';
 import { OrderItemObject } from 'src/app/shared/models/price';
 import { OrderService } from 'src/app/shared/services/order.service';
-import { WafService } from 'src/app/shared/services/waf.service';
 import { DataPayment, ItemPayment, OfferItem, Order, OrderItem } from '../../instances/instances.model';
-import { ServiceActionType, ServiceType } from 'src/app/shared/enums/common.enum';
 import { ObjectStorageService } from 'src/app/shared/services/object-storage.service';
-import { CloudBackup } from '../cloud-backup.model';
+import { CloudBackup, CloudBackupExtend } from '../cloud-backup.model';
 import { CloudBackupService } from 'src/app/shared/services/cloud-backup.service';
+import { ServiceActionType } from 'src/app/shared/enums/common.enum';
 
 @Component({
-  selector: 'one-portal-waf-extend',
-  templateUrl: './waf-extend.component.html',
-  styleUrls: ['./waf-extend.component.less'],
+  selector: 'one-portal-cloud-backup-extend',
+  templateUrl: './cloud-backup-extend.component.html',
+  styleUrls: ['./cloud-backup-extend.component.less'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CloudBackupExtendComponent implements OnInit {
@@ -81,7 +80,7 @@ export class CloudBackupExtendComponent implements OnInit {
   }
 
 
-  //CloudBackupExtend: CloudBackupExtend = new CloudBackupExtend();
+  CloudBackupExtend: CloudBackupExtend = new CloudBackupExtend();
   CloudBackupDetail: CloudBackup = new CloudBackup();
 
 
@@ -130,16 +129,14 @@ export class CloudBackupExtendComponent implements OnInit {
       });
   }
   initCloudBackupExtend() {
-    // this.CloudBackupExtend.newExpireDate = this.newExpiredDate;
-    // this.CloudBackupExtend.customerId = this.tokenService.get()?.userId;
-    // this.CloudBackupExtend.userEmail = this.tokenService.get()?.email;
-    // this.CloudBackupExtend.actorEmail = this.tokenService.get()?.email;
-    // this.CloudBackupExtend.serviceName = this.CloudBackupDetail.name;
-    // this.CloudBackupExtend.typeName =
-    //   'SharedKernel.IntegrationEvents.Orders.Specifications.GenericExtendSpecificaton,SharedKernel.IntegrationEvents, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null';
-    // this.CloudBackupExtend.serviceType = ServiceType.CloudBackup;
-    // this.CloudBackupExtend.actionType = ServiceActionType.EXTEND;
-    // this.CloudBackupExtend.serviceInstanceId = this.id;
+    this.CloudBackupExtend.newExpireDate = this.newExpiredDate;
+    this.CloudBackupExtend.customerId = this.tokenService.get()?.userId;
+    this.CloudBackupExtend.userEmail = this.tokenService.get()?.email;
+    this.CloudBackupExtend.actorEmail = this.tokenService.get()?.email;
+    this.CloudBackupExtend.serviceName = this.CloudBackupDetail.name;
+    //this.CloudBackupExtend.serviceType = ServiceType.CloudBackup;
+    this.CloudBackupExtend.actionType = ServiceActionType.EXTEND;
+    this.CloudBackupExtend.serviceInstanceId = this.id;
   }
 
   totalAmount: number = 0;
@@ -148,8 +145,8 @@ export class CloudBackupExtendComponent implements OnInit {
     this.initCloudBackupExtend();
     let itemPayment: ItemPayment = new ItemPayment();
     itemPayment.orderItemQuantity = 1;
-    itemPayment.specificationString = "";//JSON.stringify(this.CloudBackupExtend);
-    itemPayment.specificationType = 'waf_extend';
+    itemPayment.specificationString = JSON.stringify(this.CloudBackupExtend);
+    itemPayment.specificationType = 'cloud_backup_extend';
     itemPayment.serviceDuration = this.numberMonth;
     itemPayment.sortItem = 0;
     let dataPayment: DataPayment = new DataPayment();
@@ -167,23 +164,23 @@ export class CloudBackupExtendComponent implements OnInit {
   }
 
   order: Order = new Order();
-  orderItem: OrderItem[] = [];
+  orderItems: OrderItem[] = [];
   extend() {
-    this.orderItem = [];
+    this.orderItems = [];
     this.initCloudBackupExtend();
-    let specification ="";// JSON.stringify(this.CloudBackupExtend);
-    let orderItemOS = new OrderItem();
-    orderItemOS.orderItemQuantity = 1;
-    orderItemOS.specification = specification;
-    orderItemOS.specificationType = 'waf_extend';
-    orderItemOS.price = this.totalAmount;
-    orderItemOS.serviceDuration = this.numberMonth;
-    this.orderItem.push(orderItemOS);
+    let specification = JSON.stringify(this.CloudBackupExtend);
+    let orderItem = new OrderItem();
+    orderItem.orderItemQuantity = 1;
+    orderItem.specification = specification;
+    orderItem.specificationType = 'cloud_backup_extend';
+    orderItem.price = this.totalAmount;
+    orderItem.serviceDuration = this.numberMonth;
+    this.orderItems.push(orderItem);
 
     this.order.customerId = this.tokenService.get()?.userId;
     this.order.createdByUserId = this.tokenService.get()?.userId;
     this.order.note = 'Gia hạn CloudBackup';
-    this.order.orderItems = this.orderItem;
+    this.order.orderItems = this.orderItems;
     this.orderService.validaterOrder(this.order).subscribe({
       next: (data) => {
         if (data.success) {
